8349735a06bc345f17f4b3cedc226ff537790b5d
zhoujun900113 <437709293@qq.com>
2019-01-21 18:21:30 +0800

    header finish

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -17,22 +17,80 @@
             <span class="text">{{seller.supports[0].description}}</span>
           </div>
         </div>
-        <div class="support-count" v-if="seller.supports">
+        <div class="support-count" v-if="seller.supports" @click="showDetail">
           <span class="count">{{seller.supports.length}}个</span>
           <i class="sell-icon icon-keyboard_arrow_right"></i>
         </div>
       </div>
-      <div class="bulletin-wrapper"></div>
+      <div class="bulletin-wrapper" @click="showDetail">
+        <span class="bulletin-title"></span>
+        <span class="bulletin-text">{{seller.bulletin}}</span>
+        <i class="sell-icon icon-keyboard_arrow_right"></i>
+      </div>
+      <div class="background">
+        <img :src="seller.avatar"/>
+      </div>
+      <transition>
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item,index) in seller.supports" :key="index">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
+        </div>
+      </div>
+      </transition>
     </div>
 </template>
 <script>
+import Star from '@/components/star/star'
 export default {
   name: 'Header',
   props: {
     seller: Object
   },
+  data () {
+    return {
+      detailShow: false
+    }
+  },
+  methods: {
+    showDetail () {
+      this.detailShow = true
+    },
+    hideDetail () {
+      this.detailShow = false
+    }
+  },
   created () {
     this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+  },
+  components: {
+    Star
   }
 }
 </script>
@@ -44,7 +102,8 @@ export default {
     width: 100%
     height: 2.68rem
     color: #ffffff
-    background: #999
+    background: rgba(7,17,27,0.5)
+    overflow: hidden
     .content-wrapper
       position: relative
       padding: .48rem .24rem .36rem .48rem
@@ -123,4 +182,126 @@ export default {
           margin-left: .02rem
           line-height: .48rem
           font-size: .1rem
+    .bulletin-wrapper
+      position: relative
+      height: .56rem
+      line-height: .56rem
+      padding: 0 .44rem 0 .12rem
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7,17,27,0.2)
+      .bulletin-title
+        display: inline-block
+        width: .44rem
+        height: .24rem
+        background-size: .44rem .24rem
+        bg-image('bulletin')
+        background-repeat: no-repeat
+      .bulletin-text
+        font-size: .1rem
+        margin: 0 .04rem
+        vertical-align: top
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: .1rem
+        right: .12rem
+        top: .16rem
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(.1rem)
+      img
+        width: 100%
+        height: 100%
+    .v-enter, .v-leave-to
+        opacity: 0
+        background: rgba(7,17,27,0)
+    .v-enter-active, .v-leave-active
+        transition:all 2s ease
+    .detail
+      position: fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      background: rgba(7,17,27,0.8)
+      .detail-wrapper
+        min-height: 100%
+        width: 100%
+        .detail-main
+          padding-top: 1.28rem
+          padding-bottom: 1.28rem
+          .name
+            line-height: .32rem
+            text-align: center
+            font-size: .32rem
+            font-weight: 700
+          .star-wrapper
+            margin-top: .32rem
+            padding: 0.2rem 0
+            text-align: center
+          .title
+            display: flex
+            width: 80%
+            margin: .28rem auto .24rem auto
+            .line
+              flex: 1
+              position: relative
+              top: -.12rem
+              border-bottom: 0.01rem solid rgba(255,255,255,0.2)
+            .text
+              padding: 0 .24rem
+              font-size: 0.14rem
+              font-weight: 700
+          .supports
+            width: 80%
+            margin: 0 auto
+            .support-item
+              padding: 0 .24rem
+              margin-bottom: .24rem
+              font-size: 0
+              &.last-child
+                margin-bottom: 0
+              .icon
+                display: inline-block
+                width: .32rem
+                height: .32rem
+                vertical-align: top
+                margin-right: .12rem
+                background-size: .32rem .32rem
+                background-repeat: no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                line-height: .32rem
+                font-size: .24rem
+          .bulletin
+            width: 80%
+            margin: 0 auto
+            .content
+              font-size: .24rem
+              padding: 0 .24rem
+              line-height: .48rem
+      .detail-close
+        position: relative
+        width: .64rem
+        height: .64rem
+        margin: -1.28rem auto 0 auto
+        clear: both
+        font-size: .64rem
 </style>
---
+++ src/components/star/star.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="star" :class="starType">
+      <span v-for="(itemClass,index) in itemClasses" :key="index" :class="itemClass" class="star-item"></span>
+    </div>
+</template>
+<script>
+const LENGTH = 5
+const CLS_ON = 'on'
+const CLS_HALF = 'half'
+const CLS_OFF = 'off'
+
+export default {
+  name: 'Star',
+  props: {
+    size: {
+      type: Number
+    },
+    score: {
+      type: Number
+    }
+  },
+  computed: {
+    starType () {
+      return 'star-' + this.size
+    },
+    itemClasses () {
+      let result = []
+      let score = Math.floor(this.score * 2) / 2
+      let hasDecimal = score % 1 !== 0
+      let integer = Math.floor(score)
+      for (let i = 0; i < integer; i++) {
+        result.push(CLS_ON)
+      }
+      if (hasDecimal) {
+        result.push(CLS_HALF)
+      }
+      while (result.length < LENGTH) {
+        result.push(CLS_OFF)
+      }
+      return result
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+    @import "../../common/stylus/mixin"
+    .star
+        .star-item
+          display: inline-block
+          background-repeat: no-repeat
+        &.star-48
+          .star-item
+            width: .4rem
+            height: .4rem
+            margin-right: .44rem
+            background-size: .4rem .4rem
+            &:last-child
+              margin-right: 0
+            &.on
+              bg-image('star48_on')
+            &.half
+              bg-image('star48_half')
+            &.off
+              bg-image('star48_off')
+        &.star-36
+          .star-item
+            width: .3rem
+            height: .3rem
+            margin-right: .32rem
+            background-size: .3rem .3rem
+            &:last-child
+              margin-right: 0
+            &.on
+              bg-image('star36_on')
+            &.half
+              bg-image('star36_half')
+            &.off
+              bg-image('star36_off')
+        &.star-24
+          .star-item
+            width: .2rem
+            height: .2rem
+            margin-right: .06rem
+            background-size: .2rem .2rem
+            &:last-child
+              margin-right: 0
+            &.on
+              bg-image('star24_on')
+            &.half
+              bg-image('star24_half')
+            &.off
+              bg-image('star24_off')
+</style>
